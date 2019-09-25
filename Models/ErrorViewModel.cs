af9f3715fd72cc0ff3bab7337b324f1f782c3b7c
LAPTOP-NOU2PUJ1\ITZEL SOSA CRUZ <itzel_cruz_sosa@hotmail.com>
2019-09-25 16:40:06 -0500

    Subieron  archivos.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Prueba_25_02_19.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
